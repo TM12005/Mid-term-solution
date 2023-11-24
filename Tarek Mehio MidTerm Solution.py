b134917f9fed46b202acdfce49d9cef23096777a
T M <t4m1.618@live.com>
2023-11-24 22:35:34 +0200

    JSON

--- Tarek Mehio MidTerm Solution.py
+++ Tarek Mehio MidTerm Solution.py
@@ -1,7 +1,42 @@
+import json
+from urllib2 import Request, urlopen, URLError
+#this code to open (load) the json file
+mytabs = {}
+myjsonfile = open("C:/Users/t4m16/Mid-term-solution/mytabs.json", "r")
+jsondata = myjsonfile.read()
+mytabs = json.loads(jsondata)
+
+#Save a new tab in the JSON file
+def openTab(mytabs):
+    print(mytabs)
+    #inputs from the user (Title and URL)
+    tab_title = input("Enter tab name:")
+    tab_url = input("Enter URL")
+  
+    req = Request(tab_url)
+    try:
+     response = urlopen(req)
+    except URLError, e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print ('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+         print('The server couldn\'t fulfill the request.')
+         print('Error code: ', e.code)
+        else:
+            print('URL is good!')
+    #Updating the dictionary and saving the changes in the JSON
+    mytabs.update({tab_title : tab_url})
+    with open("C:/Users/t4m16/Mid-term-solution/mytabs.json", "w") as f:
+        json.dump(mytabs, f)
+    print(mytabs)
+
+
+
 
 # Main menu that display option lists
 def mainMenu():
-    
+    #List to display in terminal
     print("1. Open Tab")
     print("2. Close Tab")
     print("3. Switch Tab")
@@ -12,11 +47,12 @@ def mainMenu():
     print("8. Import Tabs")
     print("9. Exit")
     
-    user_input = int(input("Please Choose A Number:"))
-    
+    user_input = input("Please Choose A Number:")
+    #Continues While loop that exits only when user enter "9"
     while user_input != 9:
         if user_input == 1:
-            openTab()
+            openTab(mytabs)
+            mainMenu()
         elif user_input == 2:
             closeTab()
         elif user_input == 3:
